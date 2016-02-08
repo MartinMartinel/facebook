f1165b04161923a7a4da9a222c3e6f571d3a4852
Martin Martinel <martin.martinel@deventice.com>
2016-02-08 20:20:15 +0100

    configure devise
    set up Devise modules
    set after_signing, after_signup, after_update and after_signout paths
    display devise errors as flash messages (+1 squashed commit)
    Squashed commits:
    [4950e7a] mixup

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,17 @@
 Rails.application.routes.draw do
-  devise_for :users
   devise_scope :user do
     root to: 'devise/registrations#new'
   end
+
+  devise_for :users,
+             :controllers => { :registrations => "registrations" },
+             :skip => [:sessions]
+
+  as :user do
+    get    'login'  => 'devise/sessions#new',     :as => :new_user_session
+    post   'login'  => 'devise/sessions#create',  :as => :user_session
+    delete 'logout' => 'devise/sessions#destroy', :as => :destroy_user_session
+    get    'signup' => 'registrations#new'
+  end
+
 end
