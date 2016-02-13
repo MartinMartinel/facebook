680a2e72cf1b28ea85cfed02dd0b87f5c4988987
Martin Martinel <martin.martinel@deventice.com>
2016-02-13 20:01:07 +0100

    fixed design problems

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -37,6 +37,12 @@ RSpec.configure do |config|
     DatabaseCleaner.clean
   end
 
+  config.around(:each) do |example|
+    DatabaseCleaner.cleaning do
+      example.run
+    end
+  end
+
   config.include Helpers
 
   config.use_transactional_fixtures = false
